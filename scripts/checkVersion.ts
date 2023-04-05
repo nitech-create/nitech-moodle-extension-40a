b10e34a7bb5f3899a6563462a37787417f05e243
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-05 21:46:00 +0900

    change usage of script checkVersion

--- scripts/checkVersion.ts
+++ scripts/checkVersion.ts
@@ -1,33 +1,18 @@
-import * as semver from "std/semver/mod.ts";
 import JSON5 from 'json5';
 import type ManifestType from '../src/manifestType.ts';
 
-const apiEndPoint = 'https://api.github.com/repos/nitech-create/nitech-moodle-extension-40a/releases/latest';
+if(Deno.args.length < 1) {
+  console.error('Version specifier required');
+  Deno.exit(1);
+}
 
 const manifestText = await Deno.readTextFile('./src/manifest.json5');
 const manifest = JSON5.parse(manifestText) as ManifestType;
 
-const currentVer = semver.valid(manifest.version);
-if(currentVer === null) {
-  throw Error(`The version ${manifest.version} is not a valid semver version`);
-}
+const manifestVersion = `v${manifest.version}`;
+const cliVersion = Deno.args[0];
 
-const res = await fetch(apiEndPoint);
-if(!res.ok) {
-  throw Error(`Failed to fetch remote version information from ${apiEndPoint}`);
-}
-
-const latestReleaseVerStr = JSON.parse(await res.text())["tag_name"];
-if(typeof latestReleaseVerStr !== 'string') {
-  console.log('No release found.');
-  Deno.exit();
-}
-
-const latestReleaseVer = semver.valid(latestReleaseVerStr);
-if(latestReleaseVer === null) {
-  throw Error(`The remote release version ${latestReleaseVerStr} is not a valid semver version (${latestReleaseVerStr})`);
-}
-
-if(semver.lte(currentVer, latestReleaseVer)) {
-  throw Error(`The version ${manifest.version} is smaller or equals to latest release version`);
+if(manifestVersion !== cliVersion) {
+  console.error(`Version did not match: ${manifestVersion} (manifest.json5) != ${cliVersion} (${Deno.args[1] ?? 'CLI'})`);
+  Deno.exit(1);
 }
