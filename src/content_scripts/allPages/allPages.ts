660da4eb7d9d4d867833b39a936a9d0e9f82d957
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-06 16:51:06 +0900

    add function to remove queries for forced download links

---
+++ src/content_scripts/allPages/allPages.ts
@@ -0,0 +1,7 @@
+import removeForceDownload from "./removeForceDownload.ts";
+
+globalThis.addEventListener('load', async () => {
+  console.log('Extension loaded.');
+
+  await removeForceDownload.loader();
+});
