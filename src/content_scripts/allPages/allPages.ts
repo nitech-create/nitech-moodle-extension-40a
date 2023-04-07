fb7be6a97d92caecb77ebd2b816f2562635cfea1
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-07 23:42:25 +0900

    add replacer of header course name

--- src/content_scripts/allPages/allPages.ts
+++ src/content_scripts/allPages/allPages.ts
@@ -1,11 +1,13 @@
 import removeForceDownload from './removeForceDownload.ts';
 import replaceNavigationText from './replaceNavigationText.ts';
+import replaceHeaderCourseName from './replaceHeaderCourseName.ts';
 
 globalThis.addEventListener('load', async () => {
   console.log('Extension loaded.');
 
   await Promise.all([
     replaceNavigationText.loader(),
-    await removeForceDownload.loader(),
+    replaceHeaderCourseName.loader(),
+    removeForceDownload.loader(),
   ]);
 });
