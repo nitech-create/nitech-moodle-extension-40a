a2790cb6938fd5c02420be2ad484d3adafa05b7e
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-09 00:05:02 +0900

    create UI outline

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,3 +1,14 @@
+import { getOptions, storeOptionsByMerge } from "../common/storage/options.ts";
+import { renderApp } from "./App.tsx";
+
 globalThis.addEventListener('load', () => {
-  document.write('loaded');
+  getOptions().then((options) => {
+    console.log(options);
+  });
+
+  const elAppRoot = document.getElementById('app_root');
+  if(!elAppRoot) {
+    throw Error(`element #app_root is not found`);
+  }
+  renderApp(elAppRoot);
 });
