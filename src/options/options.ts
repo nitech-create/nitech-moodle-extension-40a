eca0a0ba4d0555355f766a425bb3f9d532dfa3b7
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-10 00:47:23 +0900

    remove unused statements

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,11 +1,6 @@
-import { getOptions, storeOptionsByMerge } from '../common/storage/options.ts';
 import { renderApp } from './app/App.tsx';
 
 globalThis.addEventListener('load', () => {
-  getOptions().then((options) => {
-    console.log(options);
-  });
-
   const elAppRoot = document.getElementById('app_root');
   if (!elAppRoot) {
     throw Error(`element #app_root is not found`);
